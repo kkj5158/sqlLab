816d4dd4adc69de99cc1ac38af1abd78d0e5cc31
jjk5158 <jjk5158@naver.com>
2025-05-08 10:23:54 +0900

    feat : 문제추가

--- sql-practice/levels/lv01/solution/6.sql
+++ sql-practice/levels/lv01/solution/6.sql
@@ -1,3 +1,13 @@
+## 서브 쿼리 활용해서 조회하기
 select e.name, e.salary
     from employees as e
-where
+where e.salary = (select max(salary) from employees);
+
+;
+
+
+select max(e.salary)
+    from employees as e;
+
+
+## 다른 방법으로도 풀어보기
