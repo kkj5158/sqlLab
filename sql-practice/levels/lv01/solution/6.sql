a844aa91ac094867cf12bec81836f7d370d5c7c1
jjk5158 <jjk5158@naver.com>
2025-05-13 18:02:52 +0900

    Changes

--- sql-practice/levels/lv01/solution/6.sql
+++ sql-practice/levels/lv01/solution/6.sql
@@ -3,11 +3,17 @@ select e.name, e.salary
     from employees as e
 where e.salary = (select max(salary) from employees);
 
-;
 
-
-select max(e.salary)
-    from employees as e;
+select e.name, e.salary
+    from employees as e
+where e.salary in (select max(salary) from employees);
 
 
 ## 다른 방법으로도 풀어보기
+
+select e.name, e.salary
+    from employees as e
+order by e.salary DESC
+limit 1;
+
+## 해당 방식은 동점자 처리가 어렵다. (급여가
